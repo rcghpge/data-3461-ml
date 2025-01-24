372cdb5672cf088b9fede7dba91521fb1355cddf
rcghpge <rcocker87@gmail.com>
2025-01-24 12:02:14 -0600

    mini-project 1

---
+++ mini-project-1/classification.py
@@ -0,0 +1,15 @@
+# Undergradute Classification Mini-Project #1
+
+# Example
+college_year = 3
+
+if (college_year == 1):
+    print("Freshman")
+elif (college_year == 2):
+    print("Sophmore")
+elif (college_year == 3):
+    print("Junior")
+elif (college_year == 4):
+    print("Senior")
+
+
